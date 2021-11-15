72beef8e5913c893b28b5ab8bd052c19218bf9ef
wb1110 <strengthcoachwb@gmail.com>
2021-11-15 11:31:38 -0600

    starting footer

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <footer className="App-footer">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
