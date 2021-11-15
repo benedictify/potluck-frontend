888e21351bf018bf2768ad727a8dba5462a30f6b
unknown <uxob03@outlook.com>
2021-11-15 12:47:46 -0500

    app js imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Middle from './Components/Middle';
+import Footer from './Components/Footer';
 
 function App() {
   return (
---
+++ src/Components/Middle.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Middle = () => {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <div>
+          
+      </div>
+    </div>
+  );
+};
+
+export default Middle;
