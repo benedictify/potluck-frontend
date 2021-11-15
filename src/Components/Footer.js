82f595d222bb686c7bcedc019fa28375b76b9859
wb1110 <strengthcoachwb@gmail.com>
2021-11-15 13:54:00 -0600

    updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,58 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledList = styled.ul`
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+`
+
+const StyledLink = styled.li`
+    margin: 0 2%;
+`
+
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <footer>
+                <StyledList>
+                    <StyledLink>
+                        <Link to="/">Home</Link>
+                    </StyledLink>
+                    <StyledLink>
+                        <Link to="/user-login">User Login</Link>
+                    </StyledLink>
+                    <StyledLink>
+                        <Link to="/signup-page">Signup Page</Link>
+                    </StyledLink>
+                    <StyledLink>
+                        <Link to="/events">Events</Link>
+                    </StyledLink>
+                </StyledList>
+            </footer>
+                <Routes>
+                    <Route path ="/user-login">
+                        {/* User Login */}
+                    </Route>
+                    <Route path ="/signup-page">
+                        {/* Signup-Page */}
+                    </Route>
+                    <Route path ="/events">
+                        {/* Events */}
+                    </Route>
+                    <Route exact path ="/">
+                        {/* Home */}
+                    </Route>
+                </Routes>
+        </Router>
+       
     )
 }
 
