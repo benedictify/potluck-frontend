1a0d15e3785e1d477564e7977bda25d2177aa77b
wb1110 <strengthcoachwb@gmail.com>
2021-11-15 11:51:59 -0600

    updating from main

--- src/Footer.js
+++ src/Footer.js
@@ -7,14 +7,6 @@ function Footer() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </footer>
     </div>
   );
