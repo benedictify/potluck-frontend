c6dc96dc00330a8585f41ceeec85b8d696baf097
unknown <uxob03@outlook.com>
2021-11-15 13:15:06 -0500

    created middle content with filler. going to style components

--- src/Components/Middle.js
+++ src/Components/Middle.js
@@ -1,21 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
 
 const Middle = () => {
   
-  
+
+
+
   
   
   return (
-    <div>
-      <div>
+    <div className='whole-body'>
+      <StyledBody>
+      <div className='left-div'>
         <h1>Big Pot of Luck</h1>
         <h3>This years annual potluck This years annual potluck This years annual potluck This years annual potluck This years annual potluck</h3>
         <h3>Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut Lorem wont shortcut</h3>
         <button>Get Started!</button>
       </div>
-      <div>
+      <div className='right-div'>
         <img src='https://images.unsplash.com/photo-1556910146-6121b9613959?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzJ8fHBvdCUyMGx1Y2slMjBpbWFnZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60' alt='Pot Lucky' />
       </div>
+      </StyledBody>
     </div>
   );
 };
