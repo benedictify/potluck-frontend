f3f8d0d8ef11f14c615fd2e5c6f2a05c82250638
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-11-15 16:54:30 -0600

    Updating SignUpForm stylings

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,4 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.div 
+`   background-image: url('https://253qv1sx4ey389p9wtpp9sj0-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/Dishes_at_Potluck.jpg');
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2%;
+    height: 50vh;
+    color: white;
+`
+const Styledh2 = styled.h2 
+`   font-size: 3rem;
+    text-shadow: 2px 2px 4px #190000;
+`
+const StyledErrors = styled.div 
+`   color: red;
+    font-weight: bold;
+    padding: 2%;
+`
 
 const SignUpForm = (props) => {
     const {
@@ -22,9 +42,9 @@ const SignUpForm = (props) => {
     
     return(
         <div className="formContainer">
-            <div className="formHeader">
-                <h2>Create an Account</h2>
-            </div>
+            <StyledHeader className="formHeader">
+                <Styledh2>Create an Account</Styledh2>
+            </StyledHeader>
             <form id="form-body" onSubmit={onSubmit}>
                 <label>Name&nbsp;
                     <input 
@@ -35,12 +55,28 @@ const SignUpForm = (props) => {
                         type='text'
                     />
                 </label>
+                <label>Email&nbsp;
+                    <input 
+                        value = {formValues.email}
+                        onChange = {onChange}
+                        name = 'email'
+                        type = 'email'
+                    />
+                </label>
+                <label>Password&nbsp;
+                    <input 
+                        value = {formValues.password}
+                        onChange = {onChange}
+                        name = 'password'
+                        type = 'password'
+                    />
+                </label>
                 
-                <div className='errors'>
+                <StyledErrors className='errors'>
                     <div>{formErrors.name}</div>
                     <div>{formErrors.email}</div>
                     <div>{formErrors.password}</div>
-                </div>
+                </StyledErrors>
                 <button disabled={disabled} id='order-button'>SUBMIT</button>
             </form>
         </div>
