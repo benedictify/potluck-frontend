7dc65344df8aca7cd0f8c013d5585f6aa1b1bf5d
Morgan Jones <74324320+morgankj@users.noreply.github.com>
2021-11-15 12:29:23 -0600

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div>   
+            <p id="mainLogo">Potluck Planner</p>
+            <div id="navbar">
+                <Link to="/" id="nav-home">HOME</Link>
+                <Link to="/login" id="nav-login">LOGIN</Link>
+                <Link to="/signup" id="nav-signup">SIGN UP</Link>
+                <Link to="/events" id="nav-events">EVENTS</Link>
+            </div>
         </div>
     )
 }
 
-export default Header
+export default Header;
