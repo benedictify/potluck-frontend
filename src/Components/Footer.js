aeaadb1ad0d3875edcd6dbd942b4c962c2af3507
wb1110 <strengthcoachwb@gmail.com>
2021-11-15 12:34:01 -0600

    footer outline done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+
+
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <footer>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/">User Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Signup Page</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Events</Link>
+                    </li>
+                </ul>
+            </footer>
+                <Routes>
+                    <Route path ="/user-login">
+                        {/* User Login */}
+                    </Route>
+                    <Route path ="/signup-page">
+                        {/* Signup-Page */}
+                    </Route>
+                    <Route path ="/events">
+                        {/* Events */}
+                    </Route>
+                    <Route exact path ="/">
+                        {/* Home */}
+                    </Route>
+                </Routes>
+        </Router>
+       
     )
 }
 
