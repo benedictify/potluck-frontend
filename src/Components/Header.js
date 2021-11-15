ba8c6a97bd355b6501632a4ad48c0f15280fd4b4
Morgan Jones <74324320+morgankj@users.noreply.github.com>
2021-11-15 13:16:09 -0600

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.div 
+`   display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: .5% 1%;
+    background-color: green;
+`
+
+const StyledMainLogo = styled.p 
+`   font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 0;
+    color: white;
+`
+
+const StyledNavbar = styled.div 
+`   display: flex;
+justify-content: flex-end;
+width: 100%;
+`
 
 const Header = () => {
     return (
-        <div>   
-            <p id="mainLogo">Potluck Planner</p>
-            <div id="navbar">
-                <Link to="/" id="nav-home">HOME</Link>
-                <Link to="/login" id="nav-login">LOGIN</Link>
-                <Link to="/signup" id="nav-signup">SIGN UP</Link>
-                <Link to="/events" id="nav-events">EVENTS</Link>
-            </div>
-        </div>
+        <StyledHeader>   
+            <StyledMainLogo id="mainLogo">PotluckPlanner</StyledMainLogo>
+            <StyledNavbar id="navbar">
+                <Link to="/" class="headernav">HOME</Link>
+                <Link to="/login" class="headernav">LOGIN</Link>
+                <Link to="/signup" class="headernav">SIGN UP</Link>
+                <Link to="/events" class="headernav">EVENTS</Link>
+            </StyledNavbar>
+        </StyledHeader>
     )
 }
 
