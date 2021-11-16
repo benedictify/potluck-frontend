e9dac6c0392f2f1648357415795f983f476c04f6
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-11-16 11:34:18 -0600

    Update CreateEvents and push footer down

--- src/Components/CreateEvent.js
+++ src/Components/CreateEvent.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const CreateEvent = () => <h1>Create Event</h1>;
+const StyledHeader = styled.div 
+`   background-image: url('https://hips.hearstapps.com/ell.h-cdn.co/assets/15/52/1450738482-elle-potluck-04-getty.jpg');
+background-size: cover;
+background-position: center;
+`
+
+const CreateEvent = () => {
+    return (
+        <div>
+            <StyledHeader className="headerBanner">
+                    <h2>Create Event</h2>
+            </StyledHeader>
+        </div>
+    )
+};
 
 export default CreateEvent;
