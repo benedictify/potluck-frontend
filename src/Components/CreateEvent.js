e59da62a7d98bd324d6762212a010cd1920803b0
wb1110 <strengthcoachwb@gmail.com>
2021-11-16 13:37:27 -0600

    beginnings of create event

--- src/Components/CreateEvent.js
+++ src/Components/CreateEvent.js
@@ -8,11 +8,32 @@ background-position: center;
 `
 
 const CreateEvent = () => {
+    
+    
     return (
         <div>
             <StyledHeader className="headerBanner">
                     <h2>Create Event</h2>
             </StyledHeader>
+            <form>
+                <label>Date
+                    <input type="date" id="date" name="date"/>
+                </label>
+                <label>Add food to list
+                    <input type="text" id="food" name="food"/>
+                </label>
+                <input type="submit" value="Add food to list"/>
+                <label>Time
+                    <input type="time" id="time" name="time"/>
+                </label>
+                <label>Location
+                    <input type="text" id="location" name="location"/>
+                </label>
+                <label>Guest Invitations
+                    <input type="email" id="guest" name="guest"/>
+                </label>
+                <input type="submit" value="Create Event"/>
+            </form>
         </div>
     )
 };
