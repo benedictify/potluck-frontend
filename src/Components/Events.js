7c8e4aae88e30c3de702749da9fef66fa613c69e
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-11-16 10:51:54 -0600

    Updated Events page with matching header banner

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Events = () => <h1>Events</h1>;
+const StyledHeader = styled.div 
+`   background-image: url('https://www.frameworkhomeownership.org/images/uploads/potluck-like-a-pro-large-fb-share-image.png');
+`
+
+const Events = (props) => {
+    return(
+        <StyledHeader className="headerBanner">
+                <h2>Events</h2>
+        </StyledHeader>
+    );
+};
 
 export default Events;
