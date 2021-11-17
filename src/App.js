e3a9786e10c4f6fe429c77b697a1de36ff2f3907
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-17 10:30:12 -0600

    reordered App, remove unnecessary functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
 import React, { useState, useEffect } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import axios from "axios";
 import formSchema from './validation/formSchema';
 import * as yup from 'yup';
+
 import LoginForm from './Components/Login';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -11,6 +11,8 @@ import Middle from './Components/Middle';
 import SignUpForm from './Components/SignUp';
 import EventsDashboard from './Components/EventsDashboard';
 import CreateEvent from './Components/CreateEvent';
+
+import './App.css';
 
 const initialFormErrors = {
     username: '',
@@ -21,42 +23,53 @@ const initialFormErrors = {
 function App() {
     const [eventsList, setEventsList] = useState([]); // list of all events
 
-    // Const values for SignUpForm vv
-    // const [formValues, setFormValues] = useState(initialFormValues);
+    // Const values for SignUpForm
+    const [formValues, setFormValues] = useState(initialFormValues);
     const [formErrors, setFormErrors] = useState(initialFormErrors);
     const [disabled, setDisabled] = useState(true);
-    const [users, setUsers] = useState([]);
+    // const [users, setUsers] = useState([]);
 
-    const getUsers = () => {
-        axios.get('https://potluckplanner2.herokuapp.com/api/users')
-            .then(res => {
-                setUsers(res.data.data);
-            })
-            .catch(err => console.error(err))
-    }
+    // useEffect(() => {
+    // getUsers(); 
+    // we don't need to get all users, just the one who's logging in. 
+    // the Login axios call will give us the id and other user info
+    // }, []);
+
+    // const getUsers = () => {
+    //     axios.get('https://potluckplanner2.herokuapp.com/api/users')
+    //         .then(res => {
+    //             setUsers(res.data.data);
+    //         })
+    //         .catch(err => console.error(err))
+    // };
 
     const postNewUser = newUser => {
         axios.post('https://potluckplanner2.herokuapp.com/api/auth/register', newUser)
             .then(res => {
-                setUsers([...users, res.data]);
             })
             .catch(err => console.error(err))
-            .finally(() => {
-                setFormValues(initialFormValues);
-            })
-    }
+        // .finally(() => {
+        //     setUser(initialUser);
+        // })
+    };
 
     const validate = (name, value) => {
         yup.reach(formSchema, name)
             .validate(value)
             .then(() => setFormErrors({ ...formErrors, [name]: '' }))
             .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
-    }
+    };
+
+    // Values and functions for SignUpForm
+    useEffect(() => {
+        formSchema.isValid(formValues)
+            .then(valid => setDisabled(!valid));
+    }, [formValues]);
 
     const inputChange = (name, value) => {
         validate(name, value);
         setFormValues({ ...formValues, [name]: value });
-    }
+    };
 
     const formSubmit = () => {
         const newUser = {
@@ -65,17 +78,7 @@ function App() {
             password: formValues.password
         }
         postNewUser(newUser);
-    }
-
-    useEffect(() => {
-        getUsers();
-    }, [])
-
-    useEffect(() => {
-        formSchema.isValid(formValues)
-            .then(valid => setDisabled(!valid));
-    }, [formValues])
-    // ^^ Values and functions for SignUpForm
+    };
 
     return (
         <div className="App">
