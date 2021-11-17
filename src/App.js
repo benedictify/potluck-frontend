96cc84ad1c8289d135600ed3c320136f0ae2e604
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-16 22:03:48 -0600

    formatting App.js

--- src/App.js
+++ src/App.js
@@ -12,119 +12,110 @@ import SignUpForm from './Components/SignUp';
 import EventsDashboard from './Components/EventsDashboard';
 import CreateEvent from './Components/CreateEvent';
 
-
-// SignUpForm Value Setup
-const initialFormValues = {
-  name: '',
-  email: '',
-  password: ''
-}
 const initialFormErrors = {
-  name: '',
-  email: '',
-  password: ''
+    username: '',
+    email: '',
+    password: ''
 }
-const initialUsers = [];
-const initialDisabled = true;
 
 function App() {
-	const [eventsList, setEventsList] = useState([]); // list of all events
+    const [eventsList, setEventsList] = useState([]); // list of all events
 
-  // Const values for SignUpForm vv
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [disabled, setDisabled] = useState(initialDisabled);
-  const [users, setUsers] = useState(initialUsers);
+    // Const values for SignUpForm vv
+    // const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
+    const [disabled, setDisabled] = useState(true);
+    const [users, setUsers] = useState([]);
 
-  const getUsers = () => {
-    axios.get('https://potluckplanner2.herokuapp.com/api/users')
-      .then(res => {
-        setUsers(res.data.data);
-      })
-      .catch(err => console.error(err))
-  }
-
-  const postNewUser = newUser => {
-    axios.post('https://potluckplanner2.herokuapp.com/api/auth/register', newUser)
-      .then(res => {
-        setUsers([ ...users, res.data]);
-      })
-      .catch(err => console.error(err))
-      .finally(() => {
-        setFormValues(initialFormValues);
-      })
-  }
-
-  const validate = (name, value) => {
-    yup.reach(formSchema, name)
-      .validate(value)
-      .then(() => setFormErrors({ ...formErrors, [name]: ''}))
-      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
-  }
-
-  const inputChange = (name, value) => {
-    validate(name, value);
-    setFormValues({ ...formValues, [name]: value });
-  }
-
-  const formSubmit = () => {
-    const newUser = {
-      name: formValues.name.trim(),
-      email: formValues.email,
-      password: formValues.password
+    const getUsers = () => {
+        axios.get('https://potluckplanner2.herokuapp.com/api/users')
+            .then(res => {
+                setUsers(res.data.data);
+            })
+            .catch(err => console.error(err))
     }
-    postNewUser(newUser);
-  }
 
-  useEffect(() => {
-    getUsers();
-  }, [])
+    const postNewUser = newUser => {
+        axios.post('https://potluckplanner2.herokuapp.com/api/auth/register', newUser)
+            .then(res => {
+                setUsers([...users, res.data]);
+            })
+            .catch(err => console.error(err))
+            .finally(() => {
+                setFormValues(initialFormValues);
+            })
+    }
 
-  useEffect(() => {
-    formSchema.isValid(formValues)
-      .then(valid => setDisabled(!valid));
-  }, [formValues])
-  // ^^ Values and functions for SignUpForm
+    const validate = (name, value) => {
+        yup.reach(formSchema, name)
+            .validate(value)
+            .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+            .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+    }
 
-  return (
-    <div className="App">
-      <div className='content'> {/* this div pushes the footer down*/}
-        <Header />
+    const inputChange = (name, value) => {
+        validate(name, value);
+        setFormValues({ ...formValues, [name]: value });
+    }
 
-        <Switch>
-          <Route path='/events/new' >
-						<CreateEvent eventsList={eventsList} setEventsList={setEventsList} />
-					</Route>
-          
-          <Route path='/events' component={EventsDashboard}></Route>
-          
-          <Route path='/signup'>
-            <SignUpForm 
-              formValues={formValues}
-              inputChange={inputChange}
-              formSubmit={formSubmit}
-              disabled={disabled}
-              formErrors={formErrors}
-            />
-          </Route>
-          
-          <Route path='/login'>
-            <LoginForm 
-              formValues={formValues}
-              inputChange={inputChange}
-              formSubmit={formSubmit}
-              disabled={disabled}
-              formErrors={formErrors}
-            />
-          </Route>
-          <Route exact path='/'>
-            <Middle />
-          </Route>
-        </Switch>
-      </div>     
-      <Footer />
-    </div>
-  );
+    const formSubmit = () => {
+        const newUser = {
+            name: formValues.name.trim(),
+            email: formValues.email,
+            password: formValues.password
+        }
+        postNewUser(newUser);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    useEffect(() => {
+        formSchema.isValid(formValues)
+            .then(valid => setDisabled(!valid));
+    }, [formValues])
+    // ^^ Values and functions for SignUpForm
+
+    return (
+        <div className="App">
+            <div className='content'> {/* this div pushes the footer down*/}
+                <Header />
+
+                <Switch>
+                    <Route path='/events/new' >
+                        <CreateEvent eventsList={eventsList} setEventsList={setEventsList} />
+                    </Route>
+
+                    <Route path='/events' component={EventsDashboard}></Route>
+
+                    <Route path='/signup'>
+                        <SignUpForm
+                            formValues={formValues}
+                            inputChange={inputChange}
+                            formSubmit={formSubmit}
+                            disabled={disabled}
+                            formErrors={formErrors}
+                        />
+                    </Route>
+
+                    <Route path='/login'>
+                        <LoginForm
+                            formValues={formValues}
+                            inputChange={inputChange}
+                            formSubmit={formSubmit}
+                            disabled={disabled}
+                            formErrors={formErrors}
+                        />
+                    </Route>
+                    <Route exact path='/'>
+                        <Middle />
+                    </Route>
+                </Switch>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
