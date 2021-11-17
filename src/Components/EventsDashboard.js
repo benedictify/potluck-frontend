1a0c4044a97f1fc6f5573d9119c3f5dc87bab3f5
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-17 10:49:40 -0600

    formatting, rename new event link path

--- src/Components/EventsDashboard.js
+++ src/Components/EventsDashboard.js
@@ -2,28 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const StyledHeader = styled.div 
-`   background-image: url('https://www.frameworkhomeownership.org/images/uploads/potluck-like-a-pro-large-fb-share-image.png');
+const StyledHeader = styled.div`
+   background-image: url('https://www.frameworkhomeownership.org/images/uploads/potluck-like-a-pro-large-fb-share-image.png');
 `
-const StyledEventsButtons = styled.div 
-`   display: flex;
+const StyledEventsButtons = styled.div`
+    display: flex;
     flex-direction: column;
     margin-bottom: 5%;
 `
 
-const StyledP = styled.p 
-`   padding: 0 5%;
+const StyledP = styled.p`
+   padding: 0 5%;
 `
 
 const EventsDashboard = (props) => {
-    return(
+    return (
         <div id='eventsNav'>
             <StyledHeader className="headerBanner">
-                    <h2>Events Dashboard</h2>
+                <h2>Events Dashboard</h2>
             </StyledHeader>
             <StyledEventsButtons>
                 <StyledP>To organize an event, click the "Create Event" button below! If you are an organizer on an existing event and would like to edit the date, location, or time of your event, click on the "Edit Event" button below. To view your current event invitations, click on "Your Events."</StyledP>
-                <Link to="/create-event" className="createEvent eventButton">Create Event</Link>
+                <Link to={`/events/new`} className="createEvent eventButton">Create Event</Link>
                 <Link to="/edit-event" className="editEvent eventButton">Edit Event</Link>
                 <Link to="/your-events" className="yourEvents eventButton">Your Events</Link>
             </StyledEventsButtons>
