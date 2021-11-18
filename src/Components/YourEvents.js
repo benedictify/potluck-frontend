19961f3210a6cf87eada69088510b57bb9e9bc74
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-11-18 14:32:48 -0600

    Updated styling on YourEvents

--- src/Components/YourEvents.js
+++ src/Components/YourEvents.js
@@ -6,23 +6,23 @@ import image from '../Images/yourEventsImg.jpeg';
 const StyledHeader = styled.div 
 `   background-image: url(${image});
 `
+const StyledInviteContainer = styled.div 
+`   display: flex;
+    justify-content: center;
+    margin: 0;
+`
 const StyledEventInvite = styled.div 
 `   display: flex;
     border: solid lightgrey 1px;
     padding: 1%;
     width: 40%;
     border-radius: 5px;
-    margin: 2%;
+    margin: 2% auto 4%;
     justify-content: space-around;
     align-items: flex-end;
 `
 const StyledEventInviteText = styled.div 
 `   text-align: left;
-`
-const StyledInviteContainer = styled.div 
-`   display: flex;
-    justify-content: center;
-    margin: 0;
 `
 
 const YourEvents = () => {
@@ -31,10 +31,10 @@ const YourEvents = () => {
             <StyledHeader className="headerBanner">
                     <h2>Your Events</h2>
             </StyledHeader>
-            <div>
+            <div className="yourEvents">
                 <div id="openInvitations">
                     <h3>Open Invitations</h3>
-                    <p>Note that you can only accept one invitation at a time.</p>
+                    <p id="tagline">Note that you can only accept one invitation at a time.</p>
                     <StyledInviteContainer id="invitationsContainer">
                         <StyledEventInvite className="eventInvitation">
                             <StyledEventInviteText>
@@ -51,7 +51,8 @@ const YourEvents = () => {
                         </StyledEventInvite>
                     </StyledInviteContainer>
                 </div>
-                
+
+                <div style={{ borderTop: "1px solid lightgrey ", marginLeft: 30, marginRight: 30 }}></div>
                 <div id="acceptedInvitations">
                     <h3>Accepted Invitation</h3>
                     {/* vvv This is just filler data - we will need to actually pull this data for the logged in user somehow */}
@@ -68,6 +69,7 @@ const YourEvents = () => {
                     </StyledInviteContainer>
                 </div>
 
+                <div style={{ borderTop: "1px solid lightgrey ", marginLeft: 20, marginRight: 20 }}></div>
                 <div id="yourCreatedEvents">
                     <h3>Events You Organized</h3>
                     {/* vvv This is just filler data - we will need to actually pull this data for the logged in user somehow */}
