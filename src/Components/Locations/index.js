3365f598d7be709f87c72c24c69f90bc8742a192
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-19 17:01:31 -0600

    add basic Locations component

--- src/Components/Locations/index.js
+++ src/Components/Locations/index.js
@@ -1,1 +1,13 @@
-// View All page
+// "View All" page
+
+import React from "react";
+
+const Locations = () => {
+	return (
+		<div>
+			Locations Component
+		</div>
+	)
+}
+
+export default Locations;
