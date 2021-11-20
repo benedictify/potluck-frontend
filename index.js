8ff940b0670499c0749da988d04c8f9c6fe39f67
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-20 00:52:17 -0600

    move index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import { configureStore } from '@reduxjs/toolkit'
+import './src/index.css';
+import App from './src/App';
 
 const store = configureStore({ reducer: {} });
-// add reducers to reducer object, e.g.
-// import counterReducer from '...'; 
-// configureStore({reducer: { counter: counterReducer }})
 
 ReactDOM.render(
 	<Provider store={store}>
