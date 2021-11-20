9fbd72089205710b6e4063b6a30062724518dca4
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-20 00:58:42 -0600

    testing index.html locations

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import { configureStore } from '@reduxjs/toolkit'
-import './src/index.css';
-import App from './src/App';
+import './index.css';
+import App from './App';
 
 const store = configureStore({ reducer: {} });
 
