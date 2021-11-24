0dd8b4a7a780ac0468dd457883813b2bc55c7fa4
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-23 22:02:45 -0600

    sorting imports and routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import formSchema from './validation/formSchema';
 import * as yup from 'yup';
 
+// Auths
 import SignUpForm from './Components/auths/SignUp';
 import LoginForm from './Components/auths/Login';
 import Logout from './Components/auths/Logout';
+// Home
 import Header from './Components/Home/Header';
 import Body from './Components/Home/Body';
 import Footer from './Components/Home/Footer';
+// Events
 import EventsDashboard from './Components/Events/EventsDashboard';
 import YourEvents from './Components/Events/YourEvents';
 import AddEventForm from './Components/Events/AddEventForm';
 import EditEvent from './Components/Events/EditEvent';
+// Foods
 import PickFood from './Components/Foods/PickFood';
+// Locations
 import Locations from './Components/Locations/index';
+// Route
 import PrivateRoute from './Components/Route/PrivateRoute';
 
 import './App.css';
@@ -26,10 +32,11 @@ const initialValues = {
 }
 
 function App() {
-	const [eventsList, setEventsList] = useState([]); // list of all events
+	// Events
+	const [eventsList, setEventsList] = useState([]); 
 
-	// Const values for SignUpForm
-	const [formValues, setFormValues] = useState(initialValues);
+	// Auth: values for SignUpForm
+	const [signupFormValues, setSignupFormValues] = useState(initialValues);
 	const [formErrors, setFormErrors] = useState(initialValues);
 	const [disabled, setDisabled] = useState(true);
 
@@ -42,13 +49,13 @@ function App() {
 
 	// Values and functions for SignUpForm
 	useEffect(() => {
-		formSchema.isValid(formValues)
+		formSchema.isValid(signupFormValues)
 			.then(valid => setDisabled(!valid));
-	}, [formValues]);
+	}, [signupFormValues]);
 
 	const inputChange = (name, value) => {
 		validate(name, value);
-		setFormValues({ ...formValues, [name]: value });
+		setSignupFormValues({ ...signupFormValues, [name]: value });
 	};
 
 	return (
@@ -57,28 +64,31 @@ function App() {
 				<Header />
 
 				<Switch>
+					{/* Auth */}
 					<Route path='/signup'>
 						<SignUpForm />
 					</Route>
 					<Route path='/login'>
 						<LoginForm />
 					</Route>
+					<PrivateRoute path='/logout' component={Logout}></PrivateRoute>
 
+					{/* Events */}
 					<Route path='/edit-event' component={EditEvent}></Route>
 					<Route path='/acceptedEventID' component={PickFood}></Route>
 					<Route path='/your-events' component={YourEvents}></Route>
-
-					<Route path="/locations">
-						<Locations />
-					</Route>
-
 					<PrivateRoute path='/your-events' component={YourEvents}></PrivateRoute>
 					<PrivateRoute path='/create-event' component={AddEventForm} eventsList={eventsList} setEventsList={setEventsList}>
 						{/* <AddEventForm eventsList={eventsList} setEventsList={setEventsList} /> */}
 					</PrivateRoute>
 					<PrivateRoute path='/events' component={EventsDashboard}></PrivateRoute>
-					<PrivateRoute path='/logout' component={Logout}></PrivateRoute>
 
+					{/* Locations */}
+					<Route path="/locations">
+						<Locations />
+					</Route>
+
+					{/* Home */}
 					<Route exact path='/'>
 						<Body />
 					</Route>
