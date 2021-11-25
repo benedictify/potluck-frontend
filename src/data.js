cada7f8c00151cb70eeee4199062a026110c7eb4
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-24 19:55:34 -0600

    add backend-substitute and dummy data in data.js

---
+++ src/data.js
@@ -0,0 +1,45 @@
+// fake backend
+// fake initial data
+
+// user object:
+	// id: 0,
+	// name: "",
+	// password: "",
+	// email: "",
+
+// Events object:
+	// id: 0,
+	// name: "",
+	// locationID: 0
+
+// Location object:
+	// id: 0,
+	// name: "",
+	// coordinates: {latitude: "", longitude: ""},
+
+// Food object:
+	// ...?
+
+const data = {
+	usersList: [
+		{id: 1, username: "peter", password: "123", email: "peter@schurwanz.net"},
+		{id: 2, username: "hannah", password: "123", email: "hannah@schurwanz.net"},
+		{id: 3, username: "oscar", password: "123", email: "oscar@schurwanz.net"},
+		{id: 4, username: "teddy", password: "123", email: "teddy@schurwanz.net"},
+		{id: 5, username: "rebecca", password: "123", email: "rebecca@schurwanz.net"},
+		{id: 6, username: "benedict", password: "123", email: "benedict@schurwanz.net"},
+	],
+	locationsList: [
+		{ id: 1, name: "Union Station" },
+		{ id: 2, name: "Millennium Park" },
+		{ id: 3, name: "Grant Park" },
+	],
+	eventsList: [
+		{ id: 1, name: "Happy Potluck", locationID: 1},
+		{ id: 2, name: "Scary Potluck", locationID: 2},
+		{ id: 3, name: "Boring Potluck", locationID: 3},
+		{ id: 4, name: "Sausage Fest 2021", locationID: 3},
+	],
+}
+
+export default data;
